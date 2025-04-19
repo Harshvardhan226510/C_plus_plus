44341d0f0bbf9676ef996b4e4677266e8abb43db
Harshvardhan226510 <22harshavardhan22@gmail.com>
2025-04-20 01:17:09 +0530

    03_counting sheep.cpp

---
+++ 4-loops/03_counting sheep.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+for (int i = 0 ; i < 51 ; i++)
+std::cout<<i<<" sheep\n";
+}#include <iostream>
+
+int main() {
+for (int i = 0 ; i < 51 ; i++)
+std::cout<<i<<" sheep\n";
+}
