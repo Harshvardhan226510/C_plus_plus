04c1c78164010b0385cc80b55fdf2cdcff4c2f11
Harshvardhan226510 <22harshavardhan22@gmail.com>
2025-04-20 01:00:17 +0530

    02_IPO.cpp

---
+++ 2-variables/02_IPO.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  double reddit= 34.00;
+  reddit=43.25;
+  reddit=45.85;
+  reddit=50.37;
+  reddit=56.73;
+  reddit=59.99;
+  reddit=67.64;
+
+  std::cout<<reddit;
+
+}
