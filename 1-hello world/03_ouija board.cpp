0df331bc6a549ac46c09145d19907b6393637bbd
Harshvardhan226510 <22harshavardhan22@gmail.com>
2025-04-20 00:56:15 +0530

    03_ouija board.cpp

---
+++ 1-hello world/03_ouija board.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+ std::cout << "--------------------------------------------\n";
+ std::cout << "|                                          |\n";
+ std::cout << "|            x YES        NO x             |\n";
+ std::cout << "|                                          |\n";
+ std::cout << "|             ABCDEFGHIJKLMN               |\n";
+ std::cout << "|             NOPQRSTUVWXYZ                |\n";
+ std::cout << "|                                          |\n";
+ std::cout << "|                 12356890                 |\n";
+ std::cout << "|------------------------------------------|\n";
