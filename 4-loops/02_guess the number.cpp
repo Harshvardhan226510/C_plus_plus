70344817175bf8b92bb8d9c9576e49014b0c38aa
Harshvardhan226510 <22harshavardhan22@gmail.com>
2025-04-20 01:15:59 +0530

    02_guess the number.cpp

---
+++ 4-loops/02_guess the number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+int guess=0;
+
+std::cout<<"Guess the number: ";
+std::cin>>guess;
+
+while (guess != 64) {
+ std::cout<<"Wrong guess , Guess the number: ";
+ std::cin>>guess;
+}
+
+ std::cout<<"You guessed it right !";
+}
