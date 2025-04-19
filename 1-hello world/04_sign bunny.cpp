05d356bec56e5687804fc923e6a52f7273306a75
Harshvardhan226510 <22harshavardhan22@gmail.com>
2025-04-20 00:57:36 +0530

    04_sign bunny.cpp

---
+++ 1-hello world/04_sign bunny.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+ std::cout<<"-------------------------------\n";
+ std::cout<<"|                             |\n";
+ std::cout<<"|      I LOVE YOU WIFUUUU     |\n";
+ std::cout<<"|           ♡♡♡♡♡             |\n";
+ std::cout<<"|_____________________________|\n";
+ std::cout<<"      (\\___/)  ||              \n";
+ std::cout<<"      (o . o)  ||              \n";
+ std::cout<<"      /      /                \n";
+}
